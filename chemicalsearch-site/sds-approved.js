54e3b3c9eaee36953ccda96dab36d2a5766325d6
dsrogerssojo <david.rogers@sojoindustries.com>
2026-06-08 16:00:38 -0400

    Approve chemical: test

--- chemicalsearch-site/sds-approved.js
+++ chemicalsearch-site/sds-approved.js
@@ -1,1 +1,22 @@
-
+globalThis.SDS_RECORDS = (globalThis.SDS_RECORDS || []).concat([
+  {
+    "id": "test-testtest-1780948837196",
+    "name": "test",
+    "company": "testtest",
+    "product_code": "test",
+    "use": "Pending classification",
+    "sds_number": "N/A",
+    "sds_version": "N/A",
+    "issue_date": "N/A",
+    "revision_date": "N/A",
+    "supersedes_date": "N/A",
+    "composition": "test",
+    "hfrp_info": "N/A",
+    "sds_url": "test",
+    "sds_reference": "test",
+    "approved_by": "Testing Reviewer",
+    "approved_at": "2026-06-08T20:00:37.196Z",
+    "review_notes": "",
+    "updated_at": "2026-06-08"
+  }
+]);
